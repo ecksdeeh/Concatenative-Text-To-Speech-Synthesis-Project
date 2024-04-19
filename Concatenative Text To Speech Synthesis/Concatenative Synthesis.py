00bba1944a55caf6c6a8516f9244805f3abd46fe
Adonnis <adonnisp2003@gmail.com>
2024-04-19 09:16:36 -0400

    added starter code to repository.

---
+++ Concatenative Text To Speech Synthesis/Concatenative Synthesis.py
@@ -0,0 +1,18 @@
+'''The Concatenative Synthesis method of text to speech generation converts text to simple sounds(known as phonemes) and will join
+the phonemes together in order to generate a human like speech. 
+'''
+import librosa #this will be used to process audio
+import numpy as np
+
+def load_wav(file_name):
+    #loads wav file and returns waveform and sample rate
+    audio, sr = librosa.load(file_name)
+    return audio, sr
+#adding silence between speech will help the speech seem more natural
+def add_silence(audio, silence_duration):
+    #adds a moment of silence to audio
+    silence_length = int(silence_duration * sr / 1000)  #calculates duration based on sample rate
+    silence = np.zeros(silence_length)
+    return np.concatenate((silence, audio))
+#next steps: using NLP to break down text(tokenization), looking for a database with phonemes for the algorithm to use, and more
+
