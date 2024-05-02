2ef479a8028725623e53e8a3eb54c1a7d69fcdce
Adonnis <adonnisp2003@gmail.com>
2024-05-02 08:43:33 -0400

    Created Phoneme Inventory; added sound files to generate text

--- Concatenative Text To Speech Synthesis/Concatenative Synthesis.py
+++ Concatenative Text To Speech Synthesis/Concatenative Synthesis.py
@@ -25,3 +25,19 @@ def find_phoneme(phoneme, phoneme_data):
         if entry["phoneme"] == phoneme:
             return entry["audio_file"]
     return None #phoneme not found in data
+def synthesize_speech(text, phoneme_data, silence_ms=100):
+    phonemes - process_text(text)
+    concatenated_audio = None
+    for phoneme in phonemes:
+        audio_file = find_phoneme(phoneme, phoneme_data)
+        if audio_file:
+            phoneme_audio, _ = load_wav(audio_file)
+            phoneme_audio = add_silence(phoneme_audio, silence_ms)
+            if concatenated_audio is None:
+                concatenated_audio = phoneme_audio
+            else:
+                concatenated_audio= np.concatenate((concatenated_audio, phoneme_audio))
+    return concatenated_audio
+
+text = "Hi"
+synthesized_audio = synthesize_speech(text, find_phoneme)
---
+++ Concatenative Text To Speech Synthesis/phoneme_inventory.py
@@ -0,0 +1,5 @@
+phoneme_inventory = [
+  {"phoneme": "hh", "audio_file": "phonemes/hh.wav"},
+  {"phoneme": "ih", "audio_file": "phonemes/ih.wav"},
+
+]
